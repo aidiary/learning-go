cb9302cbe02174fed8c5cffb8da9504854d9e7fe
Koichiro Mori <f2forest@gmail.com>
2024-06-01 20:28:51 +0900

    lists.goを追加

---
+++ list/lists.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	prices := []float64{10.99, 8.99}
+	fmt.Println(prices[0:1])
+	prices[1] = 9.99
+
+	prices = append(prices, 5.99)
+	prices = prices[1:]
+	fmt.Println(prices)
+}
+
+// func main() {
+// 	var productNames [4]string = [4]string{"A Book"}
+
+// 	prices := [4]float64{10.99, 9.99, 45.99, 20.00}
+// 	fmt.Println(prices)
+
+// 	productNames[2] = "A Carpet"
+
+// 	fmt.Println(productNames)
+
+// 	fmt.Println(prices[2])
+
+// 	featuredPrices := prices[1:]
+// 	featuredPrices[0] = 199.99
+// 	highlitedPrices := featuredPrices[:1]
+
+// 	fmt.Println(highlitedPrices)
+// 	fmt.Println(prices)
+// 	fmt.Println(len(highlitedPrices), cap(highlitedPrices))
+
+// 	highlitedPrices = highlitedPrices[:3]
+
+// 	fmt.Println(highlitedPrices)
+// 	fmt.Println(len(highlitedPrices), cap(highlitedPrices))
+// }
